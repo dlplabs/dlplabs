cbbf89f3b1e7f7cdfe74033450dfc8ca6d91b9cf
dlplabs <dpinheiro@sec.intragov>
2017-02-14 13:44:27 -0200

    Adicionada ação ao Botao APPS

--- assets/js/magister.js
+++ assets/js/magister.js
@@ -23,4 +23,19 @@ jQuery(document).ready(function($) {
 		}
 		return false;
 	});
+
+	$('#btn-download').click(function()
+	{
+		if( $('a[href^="'+this.attr('href')+'"]', '.mainmenu').hasClass('active')  ) {
+			// close all visible divs with the class of .section
+			current_item = $('a[href^="'+this.attr('href')+'"]', '.mainmenu');
+			$('.section:visible').fadeOut( section_hide_time, function() {
+				$('a', '.mainmenu').removeClass( 'active' );
+				$(current_item).addClass( 'active' );
+				var new_section = $( $(current_item).attr('href') );
+				new_section.fadeIn( section_show_time );
+			} );
+		}
+		return false;
+	});
 });
